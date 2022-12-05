e4c6275fe2b2efe077b74ffa9f9ceccef49e491a
BarhouchMustapha <119078838+BarhouchMustapha@users.noreply.github.com>
2022-12-05 16:31:55 +0100

    Update Contact.py

---
+++ Contact.py
@@ -0,0 +1,21 @@
+def Ajouter_contact(nom,prenom,email,numtel,adresse):
+    Id = 0
+    file = open("./Contacts.txt" ,"a+")
+    file.close()
+    file = open("./Contacts.txt", "r")
+    leng = len(file.readlines())
+    file.close()
+    file = open("./Contacts.txt" ,"a+")
+    if leng != 0:
+        Id = leng
+    Id = Id + 1
+    contact = str(Id) + ";" + nom + ";" + prenom + ";" + email + ";" + numtel + "," + adresse + "\n"
+    file.write(contact)
+def Modifier_contact(id,nom,prenom,email,numtel,adresse):
+    file = open("./Contacts.txt" ,"r")
+    lines = file.readlines()
+    file.close()
+    lines[id-1] = str(id) + ";" + nom + ";" + prenom + ";" + email + ";" + numtel + "," + adresse + "\n"
+    file = open("./Contacts.txt" ,"w")
+    file.writelines(lines)
+    file.close
